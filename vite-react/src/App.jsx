6e181f3c08b800e0007acde36c52c293f758d066
AvinashMSangle <avinashMS.3119@proton.me>
2024-11-05 08:01:27 +0000

    first part

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./Card"
+function Try(){
+ return(
+  <>
+  <Card/> 
+  </>
+ );
+  // React.createElement("p",{style:{ color:"blue"}},"qegswbhsijeqfjgfwkakkfh")
+}
+export default Try;
---
+++ vite-react/src/Card.jsx
@@ -0,0 +1,9 @@
+function Card(){
+    return(
+ <div className="Card">
+    <img src="https://placehold.co/300" alt="" />
+    <h4>asdfghjkljh</h4>
+ </div>
+    )
+}
+export default Card;
